038930006194c28aeae5a03d5fdb999d43c7daca
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2024-10-06 22:14:33 -0400

    binki-churchcenter-login-prefer-email-1.0.1

--- binki-churchcenter-login-prefer-email.user.js
+++ binki-churchcenter-login-prefer-email.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name binki-churchcenter-login-prefer-email
-// @version 1.0.0
+// @version 1.0.1
 // @homepageURL https://github.com/binki/binki-churchcenter-login-prefer-email
 // @match https://*.churchcenter.com/*
 // @require https://raw.githubusercontent.com/binki/binki-userscript-when-element-query-selector-async/0a9c204bdc304a9e82f1c31d090fdfdf7b554930/binki-userscript-when-element-query-selector-async.js
